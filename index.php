868490ff6cbcd2d66127eb958d2601d2fe1bd94f
giosciafra <francescogioscia8@gmail.com>
2024-03-18 19:39:00 +0100

    FEAT: stilizzazione completata, ESERCIZIO FINITO

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ if (($_GET['filtroParcheggio']) || ($_GET['filtroVoto'])) {
     // Filtro gli hotel in base alla presenza di parcheggio e al voto minimo
     $filteredHotels = array_filter($hotels, function ($hotel) {
 
-        // Verifica se il filtro per il parcheggio è attivo
+        // Verifico se il filtro per il parcheggio è attivo
         $filtroParcheggioAttivo = ($_GET['filtroParcheggio']);
 
         // Verifico se è stato inserito un voto minimo nel filtro
@@ -93,29 +93,30 @@ if (($_GET['filtroParcheggio']) || ($_GET['filtroVoto'])) {
 
 <body>
 
-    <div class="container">
-        <h1>Elenco Hotel</h1>
+    <div class="container d-flex flex-column align-items-center my-5 p-1 bg-body-tertiary gap-2 ">
+        <h1 class="text-danger fw-bold ">Elenco Hotel</h1>
 
         <!-- Form per filtrare gli hotel-->
-        <form method="GET">
+        <form method="GET" class="d-flex justify-content-center align-items-center flex-column gap-3 ">
             <div class="form-group">
-                <label for="filtroParcheggio">Mostra solo hotel con parcheggio:</label>
+                <label for="filtroParcheggio" class=" fw-bolder ">Mostra solo hotel con parcheggio</label>
                 <input type="checkbox" id="filtroParcheggio" name="filtroParcheggio" value="true">
             </div>
 
             <div class="form-group">
-                <label for="filtroVoto">Voto minimo:</label>
+                <label for="filtroVoto" class=" fw-bolder ">Filtra per voto minimo (da 1 a 5)</label>
                 <input type="number" id="filtroVoto" name="filtroVoto" min="1" max="5">
             </div>
 
-            <button type="submit" class="btn btn-primary">Cerca</button>
+            <button type="submit"
+                class="btn border border-4 border-info-subtle bg-warning-subtle fw-bold  ">Cerca</button>
 
         </form>
 
         <br>
 
         <table class="table">
-            <thead>
+            <thead class="table-warning">
                 <tr>
                     <th scope="col">Nome Hotel</th>
                     <th scope="col">Descrizione</th>
@@ -127,10 +128,10 @@ if (($_GET['filtroParcheggio']) || ($_GET['filtroVoto'])) {
             </thead>
             <tbody>
                 <?php foreach ($hotels as $hotel): ?>
-                    <tr>
-                        <th>
+                    <tr class="table-info">
+                        <td>
                             <?php echo $hotel['name']; ?>
-                        </th>
+                        </td>
                         <td>
                             <?php echo $hotel['description'] ?>
                         </td>
@@ -156,44 +157,47 @@ if (($_GET['filtroParcheggio']) || ($_GET['filtroVoto'])) {
         if (!empty ($filteredHotels)) {
             ?>
 
-            <h2>Risultati del filtro:</h2>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Nome Hotel</th>
-                        <th scope="col">Descrizione</th>
-                        <th scope="col">Parcheggio</th>
-                        <th scope="col">Voto</th>
-                        <th scope="col">Distanza dal centro (km)</th>
-                    </tr>
-                </thead>
-                <?php foreach ($filteredHotels as $hotel): ?>
-                    <tbody>
+            <div class="container d-flex flex-column align-items-center py-5 p-1 ">
+
+                <h1 class="pb-4 text-danger fw-bold ">Risultati del filtro</h1>
+                <table class="table">
+                    <thead class="table-warning">
                         <tr>
-                            <td>
-                                <?php echo $hotel['name']; ?>
-                            </td>
-                            <td>
-                                <?php echo $hotel['description']; ?>
-                            </td>
-                            <td>
-                                <?php echo $hotel['parking'] ? 'Disponibile' : 'Non disponibile'; ?>
-                            </td>
-                            <td>
-                                <?php echo $hotel['vote']; ?>
-                            </td>
-                            <td>
-                                <?php echo $hotel['distance_to_center']; ?>
-                            </td>
+                            <th scope="col">Nome Hotel</th>
+                            <th scope="col">Descrizione</th>
+                            <th scope="col">Parcheggio</th>
+                            <th scope="col">Voto</th>
+                            <th scope="col">Distanza dal centro (km)</th>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-            <?php
+                    </thead>
+                    <?php foreach ($filteredHotels as $hotel): ?>
+                        <tbody>
+                            <tr class="table-info">
+
+                                <td>
+                                    <?php echo $hotel['name']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $hotel['description']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $hotel['parking'] ? 'Disponibile' : 'Non disponibile'; ?>
+                                </td>
+                                <td>
+                                    <?php echo $hotel['vote']; ?>
+                                </td>
+                                <td>
+                                    <?php echo $hotel['distance_to_center']; ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+                <?php
         }
         ?>
 
-    </div>
+        </div>
 
 
 
@@ -202,10 +206,10 @@ if (($_GET['filtroParcheggio']) || ($_GET['filtroVoto'])) {
 
 
 
-    <!-- Script Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+        <!-- Script Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
 </body>
 
 </html>
